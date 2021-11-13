ffe252f3770c3b95a9f065df9ec8fa690ec429fb
ArkJzzz <arkjzzz@gmail.com>
2021-11-13 22:02:12 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -4,8 +4,9 @@ import logging
 import json
 import os
 from pathlib import Path
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+# from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
 
 logger = logging.getLogger('main')
@@ -14,18 +15,7 @@ BASE_DIR = Path.cwd()
 MEDIA_ROOT = Path() / BASE_DIR / 'media'
 
 
-def main():
-    formatter = logging.Formatter(
-        fmt='%(asctime)s %(name)s:%(lineno)d - %(message)s',
-        datefmt='%Y-%b-%d %H:%M:%S (%Z)',
-        style='%',
-    )
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-    console_handler.setLevel(logging.DEBUG)
-    logger.addHandler(console_handler)
-    logger.setLevel(logging.DEBUG)
-
+def on_reload():
     books_file = os.path.join(MEDIA_ROOT, 'books_description.json')
     with open(books_file, 'r') as my_file:
         books_description_json = my_file.read()
@@ -45,8 +35,28 @@ def main():
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+
+def main():
+    formatter = logging.Formatter(
+        fmt='%(asctime)s %(name)s:%(lineno)d - %(message)s',
+        datefmt='%Y-%b-%d %H:%M:%S (%Z)',
+        style='%',
+    )
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.DEBUG)
+    logger.addHandler(console_handler)
+    logger.setLevel(logging.DEBUG)
+
+
+
+    # server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    # server.serve_forever()
+    on_reload()
+
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
 
 
 if __name__ == '__main__':
