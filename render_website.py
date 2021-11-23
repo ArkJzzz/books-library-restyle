f11e3125c585958cb9b982f5df65efce6f52b92e
ArkJzzz <arkjzzz@gmail.com>
2021-11-23 21:55:24 +0300

    Create HTML page with books

--- render_website.py
+++ render_website.py
@@ -24,14 +24,13 @@ def on_reload():
 
     env = Environment(
         loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
+        autoescape=select_autoescape(['html', 'xml']),
     )
     template = env.get_template('template.html')
 
     rendered_page = template.render(
         books=books,
     )
-
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
@@ -48,15 +47,11 @@ def main():
     logger.addHandler(console_handler)
     logger.setLevel(logging.DEBUG)
 
-
-
-    # server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    # server.serve_forever()
     on_reload()
 
     server = Server()
     server.watch('template.html', on_reload)
-    server.serve(root='.')
+    server.serve(root='.', default_filename='index.html')
 
 
 if __name__ == '__main__':
