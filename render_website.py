f8ec2122fa8cef65f2a32c61fe5dc4c05651111a
ArkJzzz <arkjzzz@gmail.com>
2021-12-03 20:15:30 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -4,7 +4,6 @@ import logging
 import json
 import os
 from pathlib import Path
-# from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
@@ -17,24 +16,27 @@ MEDIA_ROOT = Path() / BASE_DIR / 'media'
 
 
 def on_reload():
-    books_file = os.path.join(MEDIA_ROOT, 'books_description.json')
-    with open(books_file, 'r') as my_file:
-        books_description_json = my_file.read()
-
-    books = json.loads(books_description_json)
-    books_by_columns = list(chunked(books, 2))
-
+    os.makedirs('pages', exist_ok=True)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml']),
     )
     template = env.get_template('template.html')
+    books_file = os.path.join(MEDIA_ROOT, 'books_description.json')
+    with open(books_file, 'r') as my_file:
+        books = json.loads(my_file.read())
 
-    rendered_page = template.render(
-        books_by_columns=books_by_columns
-    )
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    books_by_page = list(chunked(books, 10))
+
+    for page_num, books_on_page in enumerate(books_by_page, 1):
+        books_by_columns = list(chunked(books_on_page, 2))
+
+        rendered_page = template.render(
+            books_by_columns=books_by_columns,
+            page_num=page_num,
+        )
+        with open(f'pages/index{page_num}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def main():
@@ -53,7 +55,7 @@ def main():
 
     server = Server()
     server.watch('template.html', on_reload)
-    server.serve(root='.', default_filename='index.html')
+    server.serve(root='pages', default_filename='index1.html')
 
 
 if __name__ == '__main__':
